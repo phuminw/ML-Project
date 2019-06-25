20674527011cd95b1e4b897862852cce5ec69d84
Phumin Walaipatchara <phuminw@bu.edu>
2019-06-25 15:11:32 -0400

    RNN as a module + save/load function

--- src/RNN.py
+++ src/RNN.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import numpy as np
 
 class RNN(nn.Module):
-    def __init__(self, in_size, hidden_size):
+    def __init__(self, in_size=2, hidden_size=15):
         super(RNN, self).__init__()
         self.w1 = nn.Linear(in_size, hidden_size)
         self.w2 = nn.LSTM(hidden_size,hidden_size,2,dropout=0.05)
@@ -14,39 +14,67 @@ class RNN(nn.Module):
         self.softmax = nn.Softmax(dim=0) # For output as probability
         self.cuda() # Use GPU
 
-    def forward(self,x,hidden):
-        x = x.double().view((1,len(x))) # Preprocess x
+        self.hidden = None
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = None
+
+
+    # Expect 10? real-valued input (shape nx10) as list/ndarray both X and y
+    def fit(self,X,Y,epoch=100,lr=0.0005):
+        try:
+            # Data type assertion
+            assert(((type(X) == list) or (type(X) == np.ndarray)) and ((type(Y) == list) or (type(Y) == np.ndarray)))
+
+            # Reshape to ensure shape
+            X = torch.tensor(X, dtype=torch.float64, requires_grad=True).view((len(X),2)).cuda()
+            Y = torch.tensor(Y, dtype=torch.float64, requires_grad=True).view((len(Y),1)).cuda() 
+
+            self.optimizer = torch.optim.Adam(self.parameters(),lr=lr)
+
+            for _ in range(epoch):
+                for i in range(X.shape[0]):
+                    self.optimizer.zero_grad()
+                    pred,self.hidden = self.forward(X[i].float()) # Feed forward
+                    pred = pred.view((1,4)) # Prediction in appropiate shape
+
+                    loss = self.criterion(pred,Y[i].long()) # Loss function
+                    loss.backward(retain_graph=True)
+                    self.optimizer.step()
+            return True,self.hidden
+        except AssertionError:
+            print('Input type should be either list or ndarray.')
+
+
+    def forward(self,x):
+        x = x.view((1,len(x))) # Preprocess x
         z2 = self.w1(x).cuda() # Input to hidden 
         a2 = self.sigmoid(z2).cuda() # Output of hidden
-        z3, hid = self.w2(a2.unsqueeze(1), hidden) # Input to LSTM
+        z3, hid = self.w2(a2.unsqueeze(1), self.hidden) # Input to LSTM
         z3 = self.sigmoid(z3.squeeze(1)) # Output of LSTM
         return self.softmax(self.w3(z3.cuda()).view(4)), hid # Return prob of len(out) classes and hidden output for recurrent
 
+def save_model(model, name):
+    try:
+        torch.save(model, name)
+        return True
+    except:
+        return False
+
+def load_model(name):
+    try:
+        return torch.load(name)
+    except:
+        return None
     
+
 if __name__ == '__main__':
-    net = RNN(2, 15) # Init NN with 2 in and 15 in hidden
-    net.double()
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(net.parameters(), lr=.0005)
+    net = RNN() # Init NN with 2 in and 15 in hidden
+    # net.double() # Not necessary (Maybe)
     train = [[1,2],[1,-1],[-5,3],[-4,-4],[5,2],[-4,10]] # TODO: load input 
     train_label = [[0],[3],[1],[2],[0],[1]] # TODO: load input label
 
-    epochs = 2000
-    hidden=None
-    for _ in range(epochs):
-        for i in range(len(train)):
-            x = torch.tensor(np.array(train[i],dtype=np.float64), requires_grad=True).cuda()
-            y = torch.tensor(train_label[i]).cuda()
-            y = y.long().cuda()
+    net.fit(train,train_label) # Train with data
 
-            optimizer.zero_grad()
-            pred,hidden = net(x,hidden) # Get prediction result
-            pred = pred.view((1,4)) # Prediction in appropiate shape
-
-            loss = criterion(pred,y) # Loss function
-            loss.backward(retain_graph=True)
-            optimizer.step()
-
-    test = torch.tensor(np.array([30,20], dtype=np.float64)).cuda() # For testing
-    test = test.float().cuda()
-    print(net(test,hidden)[0])
+    test = torch.tensor([30,20]).cuda() # For testing
+    test = test.float() # Must call .float before prediction
+    print(net(test)[0])
