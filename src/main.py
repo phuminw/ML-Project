b1f48fa86de0da8defabc17a6ffe15bd309cc7c7
Phumin Walaipatchara <phuminw@bu.edu>
2019-06-26 09:47:53 -0400

    Change to smaller dataset and reduce epoch

--- src/main.py
+++ src/main.py
@@ -12,10 +12,10 @@ from sklearn.model_selection import KFold
 # import matplotlib.pyplot as plt # For plotting later
 
 # Load data from preprocessed csv
-def load_data(name="MSFT/news_and_change.csv", today=True):
+def load_data(name="GOOGL/GOOGL_filtered_labeled.csv", today=True):
     nc = pd.read_csv(name)
     headlines = nc['TITLE'].values
-    vectorizer = TfidfVectorizer(max_features=500)
+    vectorizer = TfidfVectorizer(max_features=500, stop_words='english')
     X = vectorizer.fit_transform(headlines) # TD-IDF
 
     if today:
@@ -31,14 +31,14 @@ kf = KFold(n_splits=10, random_state=None, shuffle=False)
 X, y = load_data()
 
 acc = []
-for i in range(100):
+for i in range(50):
     acc_local = [] # To store accuracy of k times from k-Fold
     for train_index, test_index in kf.split(X):
         # Load train and test data and label
         train, train_label, test, test_label = X[train_index],y[train_index] ,X[test_index], y[test_index]
     
         # Param: X,Y,epoch=100,lr=0.0005
-        net.fit(train,train_label,epoch=50)
+        net.fit(train,train_label,epoch=20)
 
         acc_fold = 0 # Counter for correct prediction
 
@@ -61,7 +61,7 @@ for i in range(100):
 #     else: # Predict to fall
 #         acc[i] = int(0==int(test_label[i])) # Update to 1 if predict correctly
 
-np.save('Accuracy',acc) # For process later
+np.save('Accuracy_GOOGL',acc) # For process later
 print(np.average(acc)) # Print out accuracy of prediction
 
 # TODO: Plot accuracy-epoch?
